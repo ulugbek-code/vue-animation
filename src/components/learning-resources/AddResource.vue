6cb5dedd387f953a3d567fae12f1ae6d88129852
Ulugbek Mannopov <umannopov3@gmail.com>
2021-09-02 22:44:20 +0500

    first commit

---
+++ src/components/learning-resources/AddResource.vue
@@ -0,0 +1,103 @@
+<template>
+  <base-dialog
+    v-if="inputIsInvalid"
+    title="Invalid Input"
+    @close="confirmError"
+  >
+    <template #default>
+      <p>At least One invalid input or empty field</p>
+      <p>Please, check one more and do it again</p>
+    </template>
+    <template #actions>
+      <base-button @click="confirmError">
+        Okay
+      </base-button>
+    </template>
+  </base-dialog>
+  <base-card>
+    <form @submit.prevent="submitData">
+      <div class="form-control">
+        <label for="title">Title</label>
+        <input type="text" ref="title" />
+      </div>
+      <div class="form-control">
+        <label for="description">Description</label>
+        <textarea
+          name="description"
+          id="description"
+          rows="3"
+          ref="description"
+        ></textarea>
+      </div>
+      <div class="form-control">
+        <label for="link">Link</label>
+        <input type="url" ref="link" />
+      </div>
+      <div>
+        <base-button type="submit">Add Resource</base-button>
+      </div>
+    </form>
+  </base-card>
+</template>
+
+<script>
+import BaseButton from '../UI/BaseButton.vue';
+export default {
+  components: { BaseButton },
+  inject: ['addResource'],
+  data() {
+    return {
+      inputIsInvalid: false
+    };
+  },
+  methods: {
+    confirmError() {
+      this.inputIsInvalid = false;
+    },
+    submitData() {
+      const enteredTitle = this.$refs.title.value;
+      const enteredDesc = this.$refs.description.value;
+      const enteredLink = this.$refs.link.value;
+
+      if (
+        enteredTitle.trim() === '' ||
+        enteredDesc.trim() === '' ||
+        enteredLink.trim() === ''
+      ) {
+        this.inputIsInvalid = true;
+        return;
+      }
+
+      this.addResource(enteredTitle, enteredDesc, enteredLink);
+    }
+  }
+};
+</script>
+
+<style scoped>
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+</style>
