8b8970bf1fc5423d9f7a7e1179eb42b5fae82685
Ulugbek Mannopov <umannopov3@gmail.com>
2021-09-05 14:02:46 +0500

    new bg

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -15,13 +15,13 @@ export default {
 header {
   width: 100%;
   height: 5rem;
-  background-color: hsl(137, 100%, 20%);
+  background-color: hsl(235, 100%, 20%);
   display: flex;
   justify-content: center;
   align-items: center;
 }
 
-header h1 {
+header h1,p {
   color: white;
   margin: 0;
 }
